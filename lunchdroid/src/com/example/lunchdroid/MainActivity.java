32472280cc3280c1b85a8505e15ece73b779d443
markusschmid <markusschmid@192.168.0.144>
2013-03-19 10:42:58 +0100

    testcommit

--- lunchdroid/src/com/example/lunchdroid/MainActivity.java
+++ lunchdroid/src/com/example/lunchdroid/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //test
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
