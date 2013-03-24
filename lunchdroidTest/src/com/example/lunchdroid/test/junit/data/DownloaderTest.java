08f8bc5bf59a8e59da259ae03fa0c05afa2261b1
maxschaf <maxschaf@hotmail.com>
2013-03-24 03:20:53 +0100

    setup test project with junit3. could not run junit4, not supported by api15?!

---
+++ lunchdroidTest/src/com/example/lunchdroid/test/junit/data/DownloaderTest.java
@@ -0,0 +1,15 @@
+package com.example.lunchdroid.test.junit.data;
+
+import junit.framework.TestCase;
+
+public class DownloaderTest extends TestCase {
+
+	public void testDownloader() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testStartDownloadString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
