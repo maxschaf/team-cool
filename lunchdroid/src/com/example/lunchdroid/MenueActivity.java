da96a49e28affb1b286500d5d0c2a4b5a564fab1
Markus Schmid <schmidi3@hotmail.com>
2013-04-11 17:32:12 +0200

    menueActivity added and started onItemClick

---
+++ lunchdroid/src/com/example/lunchdroid/MenueActivity.java
@@ -0,0 +1,13 @@
+package com.example.lunchdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenueActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menue_layout);
+	}
+	
+}
--- lunchdroid/src/com/example/lunchdroid/TabDistanceActivity.java
+++ lunchdroid/src/com/example/lunchdroid/TabDistanceActivity.java
@@ -8,12 +8,20 @@ import java.util.List;
 
 import com.example.lunchdroid.data.Restaurant;
 import com.example.lunchdroid.data.RestaurantCollection;
+import com.example.lunchdroid.geo.Locator;
+import com.example.lunchdroid.MenueActivity;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TabDistanceActivity extends ListActivity {
 	@Override
@@ -28,17 +36,23 @@ public class TabDistanceActivity extends ListActivity {
 		List<Restaurant> todaysRestaurants = RestaurantCollection.getInstance().getRestaurantsByDay(LunchdroidHelper.getDateTodayZeroTime());
 		Restaurant[] array = todaysRestaurants.toArray(new Restaurant[todaysRestaurants.size()]);
 		 setListAdapter(new ListAdapter(this, array));
-		//
-		// ListView listView = getListView();
-		// listView.setTextFilterEnabled(true);
-		//
-		// listView.setOnItemClickListener(new OnItemClickListener() {
-		// public void onItemClick(AdapterView<?> parent, View view,
-		// int position, long id) {
-		// // When clicked, show a toast with the TextView text
-		// Toast.makeText(getApplicationContext(),
-		// ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-		// }
-		// });
+		
+		 ListView listView = getListView();
+		 listView.setTextFilterEnabled(true);
+		
+		 listView.setOnItemClickListener(new OnItemClickListener() {
+			 @Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				 	Log.w("Lunchdroid", "clicked on a view ");
+				 	
+				 	// todo add arguments restaurant informations
+				 	Intent intent = new Intent(TabDistanceActivity.this,MenueActivity.class);
+
+				 	startActivity(intent);
+		 }
+
+
+		 });
 	}
 }
