c7e85db1453e3d20152ac695fd6da83c39a0cdfb
daniel1234 <d.fussenegger@student.tugraz.at>
2013-04-15 17:34:46 +0200

    menue swipe and tabs

---
+++ lunchdroid/src/com/example/lunchdroid/Fragment_So.java
@@ -0,0 +1,17 @@
+package com.example.lunchdroid;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_So extends Fragment{
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.fragment_so, container, false);
+	}
+
+}
--- lunchdroid/src/com/example/lunchdroid/MenueActivity.java
+++ lunchdroid/src/com/example/lunchdroid/MenueActivity.java
@@ -1,13 +1,34 @@
 package com.example.lunchdroid;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MenueActivity extends Activity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.menue_layout);
-	}
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class MenueActivity extends FragmentActivity {
+
+	private ViewPager mViewPager;
+	private TabsAdapter mTabsAdapter;
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.pager);
+		setContentView(mViewPager);
+		
+		final ActionBar bar = getActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
+		mTabsAdapter.addTab(bar.newTab().setText("Mo"), Fragment_Mo.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Di"), Fragment_Di.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Mi"), Fragment_Mi.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Do"), Fragment_Do.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Fr"), Fragment_Fr.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Sa"), Fragment_Sa.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("So"), Fragment_So.class, null);
+	}
 }
