f50346f12067a1bc17a450cad555286048a20724
Markus Schmid <schmidi3@hotmail.com>
2013-05-14 10:32:36 +0200

    tab selection changed, but didn't work correct

--- lunchdroid/src/com/example/lunchdroid/Day_Fragment.java
+++ lunchdroid/src/com/example/lunchdroid/Day_Fragment.java
@@ -1,6 +1,5 @@
 package com.example.lunchdroid;
 
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
@@ -9,12 +8,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 @SuppressLint("NewApi")
-public class Day_Fragment extends Fragment{
-	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+public class Day_Fragment extends Fragment {
+
+	private CharSequence day;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		ActionBar ab = getActivity().getActionBar();
+		day = ab.getSelectedTab().getText();
+
+		try {
+			TextView daytext = (TextView) getActivity().findViewById(
+					R.id.day_fragment_text);
+			daytext.setText("today is:" + day.toString());
+		} catch (Exception e) {
+
+		}
 		return inflater.inflate(R.layout.day_fragment, container, false);
 	}
 
@@ -23,12 +36,9 @@ public class Day_Fragment extends Fragment{
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		Intent intent = getActivity().getIntent();
-		ActionBar ab = getActivity().getActionBar();
-		CharSequence text = ab.getSelectedTab().getText();
+
 	}
-	
-	
 
 }
--- lunchdroid/src/com/example/lunchdroid/MenueActivity.java
+++ lunchdroid/src/com/example/lunchdroid/MenueActivity.java
@@ -1,43 +1,70 @@
 package com.example.lunchdroid;
 
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
 @SuppressLint("NewApi")
-public class MenueActivity extends FragmentActivity  implements
-ActionBar.OnNavigationListener{
+public class MenueActivity extends FragmentActivity implements
+		ActionBar.TabListener {
 
 	private ViewPager mViewPager;
 	private TabsAdapter mTabsAdapter;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		mViewPager = new ViewPager(this);
 		mViewPager.setId(R.id.pager);
 		setContentView(mViewPager);
-		
+
 		final ActionBar bar = getActionBar();
 		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
 		mTabsAdapter = new TabsAdapter(this, mViewPager);
-		mTabsAdapter.addTab(bar.newTab().setText("Mo"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("Di"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("Mi"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("Do"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("Fr"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("Sa"), Day_Fragment.class, null);
-		mTabsAdapter.addTab(bar.newTab().setText("So"), Day_Fragment.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText("Mo"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("Di"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("Mi"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("Do"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("Fr"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("Sa"), Day_Fragment.class,
+				null);
+		mTabsAdapter.addTab(bar.newTab().setText("So"), Day_Fragment.class,
+				null);
 	}
 
 	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		return false;
+		System.out.println("tab selected: " + tab.getText().toString());
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Fragment fragment = new Day_Fragment();
+		Bundle args = new Bundle();
+		args.putInt(null, tab.getPosition() + 1);
+		fragment.setArguments(args);
+		// getFragmentManager().beginTransaction()
+		// .replace(R.id.container, fragment).commit();
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
 	}
 }
