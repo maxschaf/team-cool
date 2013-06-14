ca33df7e796287a43226a30e37f09a3318c36cd1
Markus Schmid <schmidi3@hotmail.com>
2013-06-14 12:30:05 +0200

    debagable manifest

---
+++ lunchdroidTest/src/com/example/lunchdroid/test/junit/activitys/LunchdoidActivityTest.java
@@ -0,0 +1,31 @@
+package com.example.lunchdroid.test.junit.activitys;
+
+import com.example.lunchdroid.LunchdroidActivity;
+import com.jayway.android.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LunchdoidActivityTest extends
+		ActivityInstrumentationTestCase2<LunchdroidActivity> {
+	
+	private Solo solo;
+
+	public LunchdoidActivityTest() {
+		super("com.example.lunchdroid",LunchdroidActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testActivity(){
+		solo.assertCurrentActivity("Wrong Activity", LunchdroidActivity.class);
+	}
+
+}
