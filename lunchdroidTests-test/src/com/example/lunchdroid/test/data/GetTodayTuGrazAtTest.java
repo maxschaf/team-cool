81dd825e128d598ba4a655a4194052dd3c1d95af
maxschaf <maxschaf@hotmail.com>
2013-06-15 11:55:12 +0200

    started new working tes t project

---
+++ lunchdroidTests-test/src/com/example/lunchdroid/test/data/GetTodayTuGrazAtTest.java
@@ -0,0 +1,53 @@
+package com.example.lunchdroid.test.data;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+import com.example.lunchdroid.data.GetTodayTuGrazAt;
+import com.example.lunchdroid.data.RestaurantCollection;
+
+public class GetTodayTuGrazAtTest extends TestCase {
+
+	private GetTodayTuGrazAt tug;
+	
+	protected void setUp() throws Exception {
+		super.setUp();		
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void teststartDownloadMenuXml() throws Exception {
+		int sizeBefore = RestaurantCollection.getInstance().size();
+
+		Date now = new Date();
+		Date max = new Date(now.getTime() + 60000);
+		tug = new GetTodayTuGrazAt(null);
+		tug.startDownloadMenuXml();
+		
+		while (!tug.hasFinishedMenuXml() && !now.after(max)) {
+			Thread.sleep(2000);
+			now = new Date();
+		}
+
+		assertTrue(RestaurantCollection.getInstance().size() > sizeBefore);
+	}
+	
+	public void teststartDownloadMenuByDayXml() throws Exception {
+		int sizeBefore = RestaurantCollection.getInstance().size();
+
+		Date now = new Date();
+		Date max = new Date(now.getTime() + 60000);
+		tug = new GetTodayTuGrazAt(null);
+		tug.startDownloadMenuByDayXml();
+		
+		while (!tug.hasFinishedMenuByDayXml() && !now.after(max)) {
+			Thread.sleep(2000);
+			now = new Date();
+		}
+
+		assertTrue(RestaurantCollection.getInstance().size() > sizeBefore);
+	}
+
+}
