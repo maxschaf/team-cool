f558a3b2ea99de3e843cd481ff483322cb080576
maxschaf <maxschaf@hotmail.com>
2013-06-17 17:00:09 +0200

    persistent preference test

--- lunchdroid/src/com/example/lunchdroid/data/PersistentPreferences.java
+++ lunchdroid/src/com/example/lunchdroid/data/PersistentPreferences.java
@@ -7,15 +7,15 @@ import android.content.SharedPreferences.Editor;
 public class PersistentPreferences {
 	private SharedPreferences mSettings;	
 	
-	PersistentPreferences(Activity parent, String name){
+	public PersistentPreferences(Activity parent, String name){
 		mSettings = parent.getSharedPreferences(name, 0);
 	}
 	
-	boolean getState(String token){
+	public boolean getState(String token){
 		return mSettings.getBoolean(token, false);
 	}
 	
-	void setState(String token, boolean state){
+	public void setState(String token, boolean state){
 		Editor editor = mSettings.edit();
 		editor.putBoolean(token, state);
 		editor.commit();
